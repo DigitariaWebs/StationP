c4951e9ecd0ae0430b0ff45d6d301e9ff3d98748
WISSEM DIB <bouterfasassim12@gmail.com>
2026-02-17 16:40:27 +0100

    Add FAQ section to the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from '@/components/sections/Hero';
 import Trouver from '@/components/sections/Trouver';
 import Howitworks from '@/components/sections/HowitWorks';
 import AboutSection from '@/components/sections/Apropos';
-
+import FAQ from '@/components/sections/FAQ'
 export default function Home() {
   return (
     <>
@@ -12,6 +12,7 @@ export default function Home() {
       <Trouver />
       <AboutSection />
       <Howitworks />
+      <FAQ/>
       <Footer />
       
     </>
